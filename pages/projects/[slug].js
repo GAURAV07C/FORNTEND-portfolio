66f595841a6aca11fc7e79c76a4900aedcc9120e
Gaurav Kumar <gaurav989267@gmail.com>
2024-10-05 17:01:28 +0530

    clash app

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,16 +1,24 @@
 import Head from "next/head";
 
-export default function projectslug() {
+export default function ProjectSlug({ slug }) {
+  return (
+    <>
+      <Head>
+        <title>{slug}</title>
+      </Head>
 
-    
-    return <>
-        <Head>
-            <title>{slug}</title>
-        </Head>
-
-        <div className="projectslug">
-         
-        </div>
-
+      <div className="projectslug">
+        {/* render project details here */}
+      </div>
     </>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  // fetch data for the project with the given slug
+  // ...
+  return {
+    props: { slug },
+  };
 }
